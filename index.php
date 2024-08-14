90ad237372445c014fcba0f3ac5d0e29f2d6d7be
Your Name <adhaindra2@gmail.com>
2024-08-14 13:59:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<table style="border: 2px solid black; border-collapse: collapse; width: 100%; font-family: Arial, sans-serif;">
+    <thead>
+        <tr style="background-color: #4CAF50; color: white;">
+            <th style="padding: 12px; text-align: left; border: 1px solid black;">Position</th>
+            <th style="padding: 12px; text-align: left; border: 1px solid black;">URL</th>
+            <th style="padding: 12px; text-align: left; border: 1px solid black;">Title</th>
+            <th style="padding: 12px; text-align: left; border: 1px solid black;">Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr style="background-color: #f9f9f9;">
+            <td style="padding: 12px; border: 1px solid black;">1</td>
+            <td style="padding: 12px; border: 1px solid black;"><a href="https://www.nike.com/" target="_blank" style="color: #0000FF; text-decoration: none;">https://www.nike.com/</a></td>
+            <td style="padding: 12px; border: 1px solid black;">Nike. Just Do It. Nike.com</td>
+            <td style="padding: 12px; border: 1px solid black;">Nike delivers innovative products, experiences and services to inspire athletes.</td>
+        </tr>
+        <tr>
+            <td style="padding: 12px; border: 1px solid black;">2</td>
+            <td style="padding: 12px; border: 1px solid black;"><a href="https://www.instagram.com/nike/?hl=de" target="_blank" style="color: #0000FF; text-decoration: none;">https://www.instagram.com/nike/?hl=de</a></td>
+            <td style="padding: 12px; border: 1px solid black;">Nike (@nike) • Instagram photos and videos</td>
+            <td style="padding: 12px; border: 1px solid black;">255m Followers, 147 Following, 1019 Posts - See Instagram photos and videos from Nike (@nike)</td>
+        </tr>
+        <tr style="background-color: #f9f9f9;">
+            <td style="padding: 12px; border: 1px solid black;">3</td>
+            <td style="padding: 12px; border: 1px solid black;"><a href="https://twitter.com/nike" target="_blank" style="color: #0000FF; text-decoration: none;">https://twitter.com/nike</a></td>
+            <td style="padding: 12px; border: 1px solid black;">Nike - Twitter</td>
+            <td style="padding: 12px; border: 1px solid black;">Welcome to Nike FC. We're not a club. We're a community. If you love the game of football, you're a part of Nike FC. Let's change the game, ...</td>
+        </tr>
+        <tr>
+            <td style="padding: 12px; border: 1px solid black;">4</td>
+            <td style="padding: 12px; border: 1px solid black;"><a href="https://www.youtube.com/user/nike" target="_blank" style="color: #0000FF; text-decoration: none;">https://www.youtube.com/user/nike</a></td>
+            <td style="padding: 12px; border: 1px solid black;">Nike - YouTube</td>
+            <td style="padding: 12px; border: 1px solid black;">We will continue to stand up for equality and work to break down barriers for athletes* all over the world. We will do and invest more to uphold ...</td>
+        </tr>
+    </tbody>
+</table>
